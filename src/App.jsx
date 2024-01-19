d3214972b96420f463e64ef7079dcf087fa66466
Antino Labs <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-19 23:12:50 +0530

    Project 1 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { React, useState } from "react";
-
+// import { React, useState } from "react";
+import Card from "./components/card"
 function App() {
 
-  const [counter,setCounter] = useState(3)
-
-  // let counter = 3
-
-  const addValue = () => {
-    // console.log("value added", counter);
-    // counter = counter + 1
-    setCounter(counter + 1)
-  }
-  const removeValue = () => {
-    // console.log("value removed", counter);
-    // counter = counter + 1
-    setCounter(counter - 1)
-  }
-
+  
   return (
     <>
-      <h1>Deepak first react project</h1>
-      <h2>Counter Value : {counter} </h2>
-      <br />
-      <button
-      onClick={addValue}
-      >Add value</button>
-      <button
-      onClick={removeValue}>remove value</button>
-      
+    <Card />
     </>
   )
 }
